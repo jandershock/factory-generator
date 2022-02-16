ec9ec0cf6fe7cf4fc357a8b90f80db8079dd0019
jandershock <jordan.andershock@gmail.com>
2022-02-16 16:47:32 -0600

    Added reindeer array generation. Need to make code modular by placing in separate files.

---
+++ main.js
@@ -0,0 +1,32 @@
+const colorGenerator = function* () {
+    const colors = ["Blue", "Red", "Orange", "Purple", "Goldenrod", "Aquamarine", "Olive", "Azure", "Fuchsia", "Chocolate", "Salmon", "Amaranth"];
+    let counter = 0;
+    while(true){
+        yield colors[counter];
+        if (counter < colors.length - 1){
+            counter++;
+        } else {
+            counter = 0;
+        }
+    }
+}
+
+const coloredReindeerBuilder = function () {
+    const reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    const coloredReindeer = [];
+    const colorGen = colorGenerator();
+
+    // Write a for loop that looks at each reindeer
+        // Invoke factory function to create reindeer object
+        reindeer.forEach((reindeer) => {
+            // Put new reindeer object in coloredReindeer array
+            coloredReindeer.push({
+                name: `${reindeer}`, 
+                color: `${colorGen.next().value}`
+            })
+        })
+    console.log(coloredReindeer);
+
+    // Return coloredReindeer array
+    return coloredReindeer;
+}
